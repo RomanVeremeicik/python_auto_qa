26393ccfc509a651a2a84d4375eec9efdc87d00d
Romans <roman.veremeicik@gmail.com>
2025-11-14 13:10:59 +0100

    update doc string

--- test_module_6_1.py
+++ test_module_6_1.py
@@ -3,7 +3,7 @@ from module6_1 import mod1
 
 
 class TestStringMethods(unittest.TestCase):
-    """Тесты для функции mod1 из модуля module_6.1"""
+    """Tests for function mod1 from module_6.1"""
 
     def test_mod1(self):
         self.assertEqual(mod1([1, 5, 6, 9]), "positive number in list")
